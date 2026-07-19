428c3c08cd2010a22562b6e7b9cba9949e783fe5
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-19 20:15:00 +0800

    feat: add switch changing position mechanic

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ themeSwitch.addEventListener("click", function() {
   bodyTag.classList.toggle("dark")
   bodyTag.classList.toggle("light")
   if (bodyTag.classList.contains("dark")) {
+    themeSwitch.appendChild(themeInfo)
+    themeSwitch.style["padding"] = "4px 12px 4px 8px"
     themeIcon.setAttribute("src", "img/dark_mode_icon.png")
     themeInfo.textContent = "Dark"
   } else {
+    themeSwitch.appendChild(themeIcon)
+    themeSwitch.style["padding"] = "4px 8px 4px 12px"
     themeIcon.setAttribute("src", "img/light_mode_icon.png")
     themeInfo.textContent = "Light"
   }
