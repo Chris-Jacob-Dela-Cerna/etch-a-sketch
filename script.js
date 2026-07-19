ecbb217a743b84f776a2ddda8e9abb0680e8800e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-07-19 19:28:29 +0800

    feat: change icon and improve interactive design of theme switch

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 const bodyTag = document.querySelector("body");
 const themeSwitch = document.querySelector(".theme-switch");
 const themeIcon = document.getElementById("theme-icon")
+const themeInfo = document.getElementById("theme-info")
 
 themeSwitch.addEventListener("click", function() {
   bodyTag.classList.toggle("dark")
   bodyTag.classList.toggle("light")
   if (bodyTag.classList.contains("dark")) {
     themeIcon.setAttribute("src", "img/dark_mode_icon.png")
+    themeInfo.textContent = "Dark"
   } else {
     themeIcon.setAttribute("src", "img/light_mode_icon.png")
+    themeInfo.textContent = "Light"
   }
 });
